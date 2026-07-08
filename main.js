3ebc15e0232fe7811c04f1c8775e79c8f2258abc
Masa Sabobeh <masasabobeh03@gmail.com>
2026-07-08 10:35:50 +0300

    Refactor message generation logic to utilize collectedInfo object for improved structure and maintainability

--- main.js
+++ main.js
@@ -1,143 +1,53 @@
-function createCharacter(name, show) {
-  return {
-    name: name,
-    show: show,
-  };
-}
-//tv characters array
-const sitcomCharactersArr = [];
-sitcomCharactersArr.push(
-  createCharacter("Leslie Knope", "Parks and Recreation"),
-);
-sitcomCharactersArr.push(
-  createCharacter("Charlie Kelly", `It's Always Sunny in Philadelphia`),
-);
+import {
+  sitcomCharactersArr,
+  traitsArr,
+  actionsArr,
+  asciiArt,
+} from "./data.js";
 
-sitcomCharactersArr.push(createCharacter("Phil Dunphy", "Modern Family"));
-sitcomCharactersArr.push(createCharacter("Michael Scott", "The Office"));
-sitcomCharactersArr.push(
-  createCharacter("Ron Swanson", "Parks and Recreation"),
-);
-sitcomCharactersArr.push(createCharacter("Raymond Holt", "Brooklyn 99"));
-sitcomCharactersArr.push(createCharacter("Jim Halpert", "The Office"));
-sitcomCharactersArr.push(createCharacter("Troy Barnes", "Community"));
-sitcomCharactersArr.push(createCharacter("Abed Nadir", "Community"));
-sitcomCharactersArr.push(createCharacter("Amy Santiago", "Brooklyn 99"));
-
-sitcomCharactersArr.push(createCharacter("Jake Peralta", "Brooklyn 99"));
-sitcomCharactersArr.push(
-  createCharacter("April Ludgate", "Parks and Recreation"),
-);
-sitcomCharactersArr.push(
-  createCharacter("Buster Bluth", "Arrested Development"),
-);
-sitcomCharactersArr.push(
-  createCharacter("Lucille Bluth", "Arrested Development"),
-);
-
-sitcomCharactersArr.push(
-  createCharacter("Tobias Fünke", "Arrested Development"),
-);
-
-sitcomCharactersArr.push(
-  createCharacter("Michael Bluth", "Arrested Development"),
-);
-sitcomCharactersArr.push(
-  createCharacter("The Waitress", `It's Always Sunny in Philadelphia`),
-);
-sitcomCharactersArr.push(
-  createCharacter("Frank Reynolds", `It's Always Sunny in Philadelphia`),
-);
-sitcomCharactersArr.push(
-  createCharacter("Deandra Reynolds", `It's Always Sunny in Philadelphia`),
-);
-sitcomCharactersArr.push(
-  createCharacter("Janine Teagues", "Abbott Elementary"),
-);
-sitcomCharactersArr.push(createCharacter("Gregory Eddie", "Abbott Elementary"));
-
-sitcomCharactersArr.push(createCharacter("Ava Coleman", "Abbott Elementary"));
-sitcomCharactersArr.push(
-  createCharacter("Barbara Howard", "Abbott Elementary"),
-);
-sitcomCharactersArr.push(
-  createCharacter("Melissa Schemmenti", "Abbott Elementary"),
-);
-sitcomCharactersArr.push(createCharacter("Jacob Hill", "Abbott Elementary"));
-sitcomCharactersArr.push(createCharacter("Mr. Johnson", "Abbott Elementary"));
-sitcomCharactersArr.push(createCharacter("Matt Pearson", "St. Denis Medical"));
-sitcomCharactersArr.push(
-  createCharacter("Joyce Henderson", "St. Denis Medical"),
-);
-sitcomCharactersArr.push(createCharacter("Gloria Pritchett", "Modern Family"));
-sitcomCharactersArr.push(createCharacter("Alex Dunphy", "Modern Family"));
-sitcomCharactersArr.push(createCharacter("Cameron Tucker", "Modern Family"));
-sitcomCharactersArr.push(createCharacter("Erin Quinn", "Derry Girls"));
-sitcomCharactersArr.push(createCharacter("Orla McCool", "Derry Girls"));
-sitcomCharactersArr.push(createCharacter("Clare Devlin", "Derry Girls"));
-sitcomCharactersArr.push(createCharacter("Michelle Mallon", "Derry Girls"));
-sitcomCharactersArr.push(createCharacter("James Maguire", "Derry Girls"));
-sitcomCharactersArr.push(createCharacter("Joe McCool", "Derry Girls"));
-sitcomCharactersArr.push(createCharacter("Dwight Schrute", "The Office"));
-sitcomCharactersArr.push(createCharacter("Pam Beesly", "The Office"));
-sitcomCharactersArr.push(createCharacter("Ryan Howard", "The Office"));
-sitcomCharactersArr.push(createCharacter("Oscar Martinez", "The Office"));
-sitcomCharactersArr.push(createCharacter("Kelly Kapoor", "The Office"));
-//console.log(sitcomCharactersArr);
-
-//action array
-const actionsArr = [
-  "give a motivational speech to absolutely nobody",
-  "make coffee before speaking to anyone",
-  "organize one drawer for no reason",
-  "delete five old screenshots",
-  "make tea like it is a major life event",
-  "reply to one message you have been avoiding",
-  "close three browser tabs",
-  "rename one messy file",
-  "rewatch one comfort episode",
-  "change your phone wallpaper",
-  "choose a new profile picture and change nothing",
-  "make a playlist of songs you hate",
-  "watch a video from a YouTuber you do not like",
-  "look up an actor and accidentally read their entire life story",
-  "watch one random interview from 2014",
-  "watch a trailer and act like you watched the movie",
-];
-//trait array
-const traitsArr = [
-  "act confident with absolutely no plan",
-  "look tired before anything even happens",
-  "make simple things unnecessarily dramatic",
-  "turn every small problem into a full episode",
-  "ask questions nobody wanted to answer",
-  "pretend everything is under control",
-  "complain first and understand later",
-  "avoid responsibility with impressive skill",
-  "take things personally for no clear reason",
-  "give advice you clearly do not follow",
-  "create problems and then act surprised",
-  "treat every task like a personal attack",
-  "start strong and immediately get distracted",
-  "act like you know what is happening",
-];
+const collectedInfo = {
+  ascii: asciiArt,
+  sitcomCharacter: sitcomCharactersArr,
+  trait: traitsArr,
+  action: actionsArr,
+};
 
 const generateRandomNumber = (lengthOf) => {
   return Math.floor(Math.random() * lengthOf);
 };
 
 //create message
-const createRandomMessage = () => {
-  const randomCharacter =
-    sitcomCharactersArr[generateRandomNumber(sitcomCharactersArr.length)];
-  //   console.log(randomCharacter);
-  const randomTrait = traitsArr[generateRandomNumber(traitsArr.length)];
-  //   console.log(randomTrait);
-  const randomAction = actionsArr[generateRandomNumber(actionsArr.length)];
-  //   console.log(randomAction);
+const createRandomMessage = (randomMessage) => {
+  for (let p in collectedInfo) {
+    const randomIndex = generateRandomNumber(collectedInfo[p].length);
+    switch (p) {
+      case "sitcomCharacter":
+        randomMessage.push(
+          `Today you are ${collectedInfo[p][randomIndex].name} from ${collectedInfo[p][randomIndex].show},`,
+        );
+        break;
+      case "trait":
+        randomMessage.push(`because you both ${collectedInfo[p][randomIndex]}`);
+        break;
+      case "action":
+        randomMessage.push(`You should ${collectedInfo[p][randomIndex]}`);
+        break;
+      case "ascii":
+        randomMessage.push(collectedInfo[p][randomIndex]);
+        break;
 
-  return `Today you are ${randomCharacter.name} from ${randomCharacter.show}, because you both ${randomTrait}.
-You should ${randomAction}.`;
+      default:
+        randomMessage.push("something wrong happened!");
+    }
+  }
 };
 
-console.log(createRandomMessage());
+function formatMessage(randomMessage) {
+  const formatted = randomMessage.join("\n");
+  return formatted;
+}
+
+let message = [];
+createRandomMessage(message);
+const finalMessage = formatMessage(message);
+console.log(finalMessage);
